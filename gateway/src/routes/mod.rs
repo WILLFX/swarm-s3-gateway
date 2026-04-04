90de4ab701fea77a7bee5a0e452ece23ee494781
WILLFX <161107986+WILLFX@users.noreply.github.com>
2026-04-04 15:23:27 +0300

    Add module declarations for bucket and object routes

--- gateway/src/routes/mod.rs
+++ gateway/src/routes/mod.rs
@@ -1,1 +1,8 @@
-﻿
+﻿pub mod create_bucket;
+pub mod delete_bucket;
+pub mod delete_object;
+pub mod get_object;
+pub mod head_object;
+pub mod list_buckets;
+pub mod list_objects_v2;
+pub mod put_object;
