42c630122b99e6e6899fe4a3f02c5c78d7bca561
WILLFX <161107986+WILLFX@users.noreply.github.com>
2026-04-05 10:41:02 +0300

    Define shared types and structs in types.rs
    Added types and structures for ChainRegistryEntry and AwsPrincipal.

--- common/src/types.rs
+++ common/src/types.rs
@@ -1,1 +1,22 @@
-// common crate shared types
+/use serde::{Deserialize, Serialize};
+
+pub type AccessKeyHash = [u8; 32];
+pub type SubstrateAddress32 = [u8; 32];
+pub type BucketId = [u8; 32];
+pub type ObjectKeyId = [u8; 32];
+pub type Etag = [u8; 32];
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct ChainRegistryEntry {
+    pub owner: SubstrateAddress32,
+    pub encrypted_sigv4_secret: Vec<u8>,
+    pub nonce: Vec<u8>,
+    pub key_version: u32,
+    pub enabled: bool,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct AwsPrincipal {
+    pub access_key_id: String,
+    pub owner: SubstrateAddress32,
+}/ common crate shared types
