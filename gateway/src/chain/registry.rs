3a4594b0cbfdd78bdfbc0581f43658e7546e6c9e
WILLFX <161107986+WILLFX@users.noreply.github.com>
2026-04-09 19:44:22 +0300

    Update registry.rs

--- gateway/src/chain/registry.rs
+++ gateway/src/chain/registry.rs
@@ -1,17 +1,1 @@
-let feed = state
-    .bee_client
-    .put_object_and_update_pointer(&bucket, &key, body.clone())
-    .await?;
 
-state
-    .anchor_client
-    .submit_anchor_object(
-        principal.owner,
-        bucket_id,
-        object_key_id,
-        feed.soc_reference,
-        bucket_manifest_root,
-        body.len() as u64,
-        etag,
-    )
-    .await?;
