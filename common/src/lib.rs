20b78e1f7557f0cd9c5d6ac109fdd4fa916daa86
WILLFX <161107986+WILLFX@users.noreply.github.com>
2026-04-20 09:33:16 +0300

    Refactor lib.rs to remove duplicate declarations
    Removed duplicate module declarations and extern crate alloc.

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -1,5 +1,15 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
+extern crate alloc;
+
+#[cfg(feature = "std")]
+pub mod error;
+
+pub mod ids;
+pub mod types;
+
+#![cfg_attr(not(feature = "std"), no_std)]
+
 #[cfg(feature = "std")]
 pub mod error;
 
